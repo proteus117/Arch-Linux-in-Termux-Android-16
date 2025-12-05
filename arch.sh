cefbd483f83910725c19c5a221553d70943a749d
proteus117 <118563901+proteus117@users.noreply.github.com>
2025-12-05 18:16:49 -0500

    Change directory structure for resolv.conf and tmp

--- arch.sh
+++ arch.sh
@@ -60,10 +60,10 @@ install1() {
         cd "$cur"
         echo "[${time1}] Extraction done."
 
-        mkdir -p "$directory/etc"
-        printf "nameserver 8.8.8.8\nnameserver 1.1.1.1\n" > "$directory/etc/resolv.conf" || true
+        mkdir -p arch-fs/etc
+        printf "nameserver 8.8.8.8\nnameserver 1.1.1.1\n" > arch-fs/etc/resolv.conf || true
         
-        mkdir -p "$directory/tmp"
+        mkdir -p arch-fs/tmp
 
     fi
 
